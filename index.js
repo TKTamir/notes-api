64c81893a3be91b739d5e9eb69dc588e8140f11f
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-29 16:35:10 +0200

    Create skeleton for API endpoints

--- index.js
+++ index.js
@@ -1,1 +1,103 @@
-console.log('Hello, Node!');
+const express = require('express');
+const app = express();
+
+//Get requests
+
+//GET Main page
+app.get('/', (req, res) => {
+  res.send('Welcome to Notes API');
+});
+
+//GET Documentation
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { roote: __dirname });
+});
+
+//GET all notes of a user by user ID (including the note contents and date).
+app.get('/users/notes', (req, res) => {
+  res.json(notes);
+});
+
+//GET a note by note ID.
+app.get('/users/notes/noteID', (req, res) => {
+  res.json(userNoteById);
+});
+
+//GET a user’s info by username.
+app.get('/users/:Username', (req, res) => {
+  res.json(userInfoByUserName);
+});
+
+//GET a user’s info by user ID.
+app.get('/users/:uaerID', (req, res) => {
+  res.json(userInfoByUserID);
+});
+
+//GET a list of all users.
+app.get('/users', (req, res) => {
+  res.json(allUsers);
+});
+
+//POST a new user account.
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username });
+  Users.create({ Username: req.body.Username, Password: hashedPassword, Email: req.body.Email });
+});
+
+//POST a new note.
+app.post('/users/notes', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.body.Username },
+    { $push: { notes: req.params.noteID } },
+    { new: true } // This line makes sure that the updated document is returned
+  );
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  };
+});
+
+//Maybe I need to add a put method to update the notes, need to decide on method.
+
+//DELETE allow users to delete notes
+app.delete(
+  '/users/:Username/notes/:noteID',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      {
+        $pull: { notes: req.params.noteID },
+      },
+      { new: true },
+      (err, updatedUser) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
+  }
+);
+//DELETE allow users to delete their account
+//DELETE- delete- Delete a user
+app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(201).send(req.params.Username + ' was deleted');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
